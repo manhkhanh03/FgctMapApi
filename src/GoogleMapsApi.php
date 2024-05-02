da6e3b4f6242aba6a86edd1993849ad8e22607f7
manhkhanh0705 <khanhmanh0705@gmail.com>
2024-05-02 09:11:14 +0700

    finally

---
+++ src/GoogleMapsApi.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace GoogleMaps\Api;
+
+use Error;
+use GuzzleHttp\Client;
+
+class GoogleMapsApi
+{
+    private $apiKey = "";
+    protected $client;
+    public function __construct(string $key)
+    {
+        $this->apiKey = $key;
+        $this->client = new Client();
+    }
+    public function autocomplete(string $input, array $optionParams = [], $output = "json")
+    {
+        $uri = "https://maps.googleapis.com/maps/api/place/autocomplete/$output?input=$input&key=$this->apiKey";
+
+        $uri .= isset($optionParams['components']) !== 0 ? "components=" . $optionParams['components'] : null;
+        $uri .= isset($optionParams['language']) !== 0 ? "language=" . $optionParams['language'] : null;
+        $uri .= isset($optionParams['location']) !== 0 ? "location=" . $optionParams['location'] : null;
+        $uri .= isset($optionParams['locationbias']) !== 0 ? "locationbias=" . $optionParams['locationbias'] : null;
+        $uri .= isset($optionParams['locationrestriction']) !== 0 ? "locationrestriction=" . $optionParams['locationrestriction'] : null;
+        $uri .= isset($optionParams['offset']) !== 0 ? "offset=" . $optionParams['offset'] : null;
+        $uri .= isset($optionParams['origin']) !== 0 ? "origin=" . $optionParams['origin'] : null;
+        $uri .= isset($optionParams['radius']) !== 0 ? "radius=" . $optionParams['radius'] : null;
+        $uri .= isset($optionParams['region']) !== 0 ? "region=" . $optionParams['region'] : null;
+        $uri .= isset($optionParams['sessiontoken']) !== 0 ? "sessiontoken=" . $optionParams['sessiontoken'] : null;
+        $uri .= isset($optionParams['strictbounds']) !== 0 ? "strictbounds=" . $optionParams['strictbounds'] : null;
+        $uri .= isset($optionParams['types']) !== 0 ? "types=" . $optionParams['types'] : null;
+
+        $res = $this->client->get($uri);
+        return $res->getBody();
+    }
+
+    public function computeRoutes(array $body, string $fielMask)
+    {
+        $url = "https://routes.googleapis.com/directions/v2:computeRoutes";
+
+        $response = $this->client->post($url, [
+            'json' => $body,
+            'headers' => [
+                'X-Goog-Api-Key' => $this->apiKey,
+                'X-Goog-FieldMask' => $fielMask,
+            ],
+        ]);
+
+        return json_decode($response->getBody());
+    }
+
+    // public function geocoding(string $address = "", string $bounds = "", string $language = "", string $region = "")
+    // {
+    //     $client = new Client();
+    //     $uri = "https://maps.googleapis.com/maps/api/geocode/json?address=$address";
+
+    //     $uri .= strlen($bounds) !== 0 ? "bounds=$bounds" : null;
+    //     $uri .= strlen($language) !== 0 ? "language=$language" : null;
+    //     $uri .= strlen($region) !== 0 ? "region=$region" : null;
+
+    //     $uri .= "key=$this->apiKey";
+    //     $res = $client->get($uri);
+
+    //     return json_decode($res->getBody());
+    // }
+    // public function geocoding(string $address = "", string $bounds = "", string $language = "", string $region = "")
+    public function geocoding(object $param, $output = "json")
+    {
+        $client = new Client();
+        $uri = "https://maps.googleapis.com/maps/api/geocode/$output?key=$this->apiKey";
+
+        $isActive = 0;
+
+        $isActive = isset($param->address) ? 1 : $isActive;
+        $isActive = isset($param->components) ? 2 : $isActive;
+
+        if ($isActive > 0) {
+            $uri .= isset($param->bounds) !== 0 ? "bounds=$param->bounds" : null;
+            $uri .= isset($param->language) !== 0 ? "language=$param->language" : null;
+            $uri .= isset($param->region) !== 0 ? "region=$param->region" : null;
+            $res = $client->get($uri);
+
+            return json_decode($res->getBody());
+        }
+
+        $missing = $isActive == 1 ? "address" : "components";
+
+        return new Error("Missing parameter $missing");
+    }
+}
